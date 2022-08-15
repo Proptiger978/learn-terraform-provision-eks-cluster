f9d4dabe7817ce21d5faf5a4c5ccaddd0a5e356d
Proptiger978 <96003620+Proptiger978@users.noreply.github.com>
2022-08-15 17:09:11 +0530

    Update eks-cluster.tf
    node name change
    instance type change
    size change

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -19,13 +19,13 @@ module "eks" {
 
   eks_managed_node_groups = {
     one = {
-      name = "node-group-1"
+      name = "pravin-node-1"
 
-      instance_types = ["t3.small"]
+      instance_types = ["t2.small"]
 
       min_size     = 1
-      max_size     = 3
-      desired_size = 2
+      max_size     = 2
+      desired_size = 1
 
       pre_bootstrap_user_data = <<-EOT
       echo 'foo bar'
@@ -37,9 +37,9 @@ module "eks" {
     }
 
     two = {
-      name = "node-group-2"
+      name = "Pravin-node-2"
 
-      instance_types = ["t3.medium"]
+      instance_types = ["t2.medium"]
 
       min_size     = 1
       max_size     = 2
